b71b788e1aeb8f90e7b6f9dfc46f4d4d60786a9c
Vishwanath-1992Vk <korimathvr@gmail.com>
2022-11-10 11:50:12 +0530

    testng

---
+++ Web-Automation/src/com/TestNGActitime/programs/Assignment6.java
@@ -0,0 +1,153 @@
+package com.TestNGActitime.programs;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment6 {
+	public static WebDriver oBrowser =null;
+	public static void main(String[] args) {
+		launchBrowser();
+		navigate();
+		login();
+		minimizeFlyOutwindow();
+		addCustomer();
+		creatProject() ;
+		deleteProject();
+		logout();
+		closeApplication();
+	}
+
+	private static void launchBrowser()
+	{
+		try
+		{
+			System.setProperty("webdriver.chrome.driver" ,
+					"C:\\ExampleAutomation\\Automation\\Web-Automation\\Library\\drivers\\chromedriver.exe");
+			oBrowser = new ChromeDriver();
+		}catch(Exception e)
+
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void navigate()
+	{ 
+		try
+		{
+			oBrowser.get("http://localhost/login.do");
+			Thread.sleep(5000);
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+	private static void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id=\'loginButton\']/div")).click();
+			Thread.sleep(5000);
+		}catch(Exception e)
+		{                                                    
+			e.printStackTrace();
+		}
+	}
+
+	private static void minimizeFlyOutwindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private static void addCustomer()
+	{                                                  
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='topnav']/tbody/tr/td[3]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='Add New'] ")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[1]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("customerLightBox_nameField")).sendKeys("Akash");
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"customerLightBox_commitBtn\"]/div/span")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private static void creatProject()                                 
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[3]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='Add New'] ")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[2]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("projectPopup_projectNameField")).sendKeys("Tiger");
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"projectPopup_commitBtn\"]/div/span")).click();
+			Thread.sleep(3000);
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void deleteProject()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[2]/div[1]/div[4]/div/div")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[2]/div[4]/div/div[3]/div")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"customerPanel_deleteConfirm_submitBtn\"]/div")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id='logoutLink']")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	private static void closeApplication()
+	{
+		try
+		{
+			oBrowser.quit(); 
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
